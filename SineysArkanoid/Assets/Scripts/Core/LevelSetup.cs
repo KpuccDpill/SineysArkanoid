9d252325ca9851f4b714c8fbc7cb3f93a10bf670
Ilya Solomoychenko <i.solomoychenko@dpill.games>
2024-09-09 22:37:27 +0300

    random level generation

--- SineysArkanoid/Assets/Scripts/Core/LevelSetup.cs
+++ SineysArkanoid/Assets/Scripts/Core/LevelSetup.cs
@@ -6,36 +6,41 @@ public class LevelSetup : MonoBehaviour
 {
     [SerializeField] private Plank plankPrefab;
 
-    [Inject] private Caret _caret;
     [Inject] private Level _level;
-    [Inject] private PlayerStats _playerStats;
 
     private const float PlankSizeX = 5;
     private const float PlankSizeY = 2;
+    private const int MaxGridSize = 7;
 
     private void Start()
     {
-        int[][] cells = new int[][]
+        List<List<int>> cells = new List<List<int>>(MaxGridSize);
+
+        for (var i = 0; i < cells.Capacity; i++)
         {
-            new[] { 0 },
-            new[] { 0 },
-            new[] { 0 },
-            new[] { 0 },
-            new[] { 0 },
-            new[] { 1 },
-        };
+            int cellsAmount = Random.Range(1, MaxGridSize + 1);
+            cells.Add(new List<int>(cellsAmount));
+
+            for (int j = 0; j < cellsAmount; j++)
+            {
+                float random = Random.value;
+                int result = random > 0.25f ? 1 : 0;
+                
+                cells[i].Add(result);
+            }
+        }
 
         var plankXSize = plankPrefab.transform.localScale.x * PlankSizeX;
         var plankYSize = -plankPrefab.transform.localScale.y * PlankSizeY;
 
         List<Plank> planks = new List<Plank>();
 
-        for (int i = 0; i < cells.Length; i++)
+        for (int i = 0; i < cells.Count; i++)
         {
-            var firstPlankPosX = -plankXSize * (cells[i].Length - 1) / 2;
+            var firstPlankPosX = -plankXSize * (cells[i].Count - 1) / 2;
             var plankPosY = plankYSize * i;
             
-            for (int j = 0; j < cells[i].Length; j++)
+            for (int j = 0; j < cells[i].Count; j++)
             {
                 if (cells[i][j] == 1)
                 {
